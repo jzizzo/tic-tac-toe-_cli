d58bb67aae4897f160da906b72976e9d78135fd9
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-06-19 15:12:46 -0700

    (feat/refactor) add git ignore, update index and package to use prompt instead of commander

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 #!/usr/bin/env node
 'use strict';
 
-const program = require('commander');
+const program = require('prompt');
 
+class TicTacToe {
+  constructor() {
+    this.board = {
+      '1': 0,
+      '2': 0,
+      '3': 0,
+      '4': 0,
+      '5': 0,
+      '6': 0,
+      '7': 0,
+      '8': 0,
+      '9': 0,
+    }
+  }
+
+}
+console.log('hello')
