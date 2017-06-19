517aa84db2fd3822e7e8a65e393a1f78c13a3262
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-06-19 15:31:05 -0700

    (feat) Show board

--- index.js
+++ index.js
@@ -18,5 +18,20 @@ class TicTacToe {
     }
   }
 
+  printBoard() {
+    var keys = Object.keys(this.board);
+    var newBoard = [];
+    for (var y = 0; y < 9; y = y + 3) {
+      var row = [];
+      for (var x = y; x < y + 3; x++) {
+        row.push(x + 1)
+      }
+      newBoard.push(row)
+      console.log(row)
+    }
+  // console.log(newBoard)
+  }
 }
-console.log('hello')
+
+var test = new TicTacToe();
+test.printBoard();
